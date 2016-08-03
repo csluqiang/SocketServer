7686cc69e639a93440fe9d02a2355bd929c382fe
卢强 <qiang.lu@renren-inc.com>
2016-08-03 16:36:06 +0800

    first commit

---
+++ SocketServer/SocketServerManager.h
@@ -0,0 +1,13 @@
+//
+//  SocketServerManager.h
+//  SocketServer
+//
+//  Created by lasso on 16/8/3.
+//  Copyright © 2016年 lasso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SocketServerManager : NSObject
+
+@end
---
+++ SocketServer/SocketServerManager.m
@@ -0,0 +1,13 @@
+//
+//  SocketServerManager.m
+//  SocketServer
+//
+//  Created by lasso on 16/8/3.
+//  Copyright © 2016年 lasso. All rights reserved.
+//
+
+#import "SocketServerManager.h"
+
+@implementation SocketServerManager
+
+@end
