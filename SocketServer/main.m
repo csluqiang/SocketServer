0d063b0426f663adf311ef4a4495ae38ae0852c4
卢强 <qiang.lu@renren-inc.com>
2016-08-03 15:03:16 +0800

    Initial Commit

---
+++ SocketServer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SocketServer
+//
+//  Created by lasso on 16/8/3.
+//  Copyright © 2016年 lasso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
